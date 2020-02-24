003b0ff1273520dc617abd549d75ee4bbb394e86
yc3604 <yc3604@tc.columbia.edu>
2020-02-23 23:13:56 -0500

    initial commit

---
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    private Vector3 offset = new Vector3(30,0,10);
+    private Vector3 rotate = new Vector3(0, -90, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.Rotate(rotate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = plane.transform.position + offset;
+        
+    }
+}
---
+++ Assets/Challenge 1/Scripts/PropellerMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropellerMove : MonoBehaviour
+{
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Time.deltaTime * new Vector3(0, 0, 10000));
+    }
+}
